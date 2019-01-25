4770ca443035cb8ae639b2462a422aa104203a82
Alain Benbassat <alain@businessandcode.eu>
2019-01-25 16:27:37 +0100

    adds api to clear newsletter preferences for opted out contacts

--- api/v3/Kunsten/Clearnewsletterpreferences.php
+++ api/v3/Kunsten/Clearnewsletterpreferences.php
@@ -6,19 +6,35 @@ function _civicrm_api3_kunsten_Clearnewsletterpreferences_spec(&$spec) {
 
 function civicrm_api3_kunsten_Clearnewsletterpreferences($params) {
   // remove all newsletter preferences from contacts who opted out
+  // FYI: solving this with 1 update statement conflicts with triggers
   $sql = "
-    UPDATE
-      civicrm_value_kunstenpunt_communicatie
-    SET
-      kunstenpunt_nieuws = ''
-      , flanders_arts_institute_news = ''
-      , initiatieven_themas = ''
+    SELECT
+      id
+    FROM
+      civicrm_contact
     WHERE
-      (kunstenpunt_nieuws <> '' or flanders_arts_institute_news <> '' or initiatieven_themas <> '')
-    AND 
-      entity_id in (select id from civicrm_contact where is_opt_out = 1)
+      is_opt_out = 1
   ";
-  CRM_Core_DAO::executeQuery($sql);
+  $dao = CRM_Core_DAO::executeQuery($sql);
+
+  while ($dao->fetch()) {
+    $sqlUpdate = "
+      UPDATE
+        civicrm_value_kunstenpunt_communicatie
+      SET
+        kunstenpunt_nieuws = ''
+        , flanders_arts_institute_news = ''
+        , initiatieven_themas = ''
+      WHERE
+        (kunstenpunt_nieuws <> '' or flanders_arts_institute_news <> '' or initiatieven_themas <> '')
+      AND 
+        entity_id = %1
+    ";
+    $sqlUpdateParams = [
+      1 => [$dao->id, 'Integer'],
+    ];
+    CRM_Core_DAO::executeQuery($sqlUpdate, $sqlUpdateParams);
+  }
 
   return civicrm_api3_create_success('OK', $params, 'Kunsten', 'Clearnewsletterpreferences');
 }
